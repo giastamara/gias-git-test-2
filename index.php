178f2054388360222673eec03a3a950a5976a7c0
giastamara <giashegy18@gmail.com>
2023-08-02 10:20:58 +0700

    Menambahkan paragraph

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed fugit alias modi excepturi at quibusdam, cumque, soluta, illo rem aspernatur labore quidem. Impedit, quidem dolore! Aliquid quaerat aliquam odit dolores.</p>
 </body>
 </html>
